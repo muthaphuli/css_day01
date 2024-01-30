0fcfba106aed98dc6c2f38909ef6025fe088df97
Winston <122846337+muthaphuli@users.noreply.github.com>
2024-01-30 10:36:33 +0200

    Add files via upload

---
+++ untitled9.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 09:47:51 2024
+
+@author: winston
+"
+""
+import pandas as pd
+
+df = pd.read_csv('iris.csv')
+
+url = 'http://...'
+df = pd.read_csv(url)
+
+column_names = ['name1', 'name2', 'name3']
+
+
+"""
+
+$abc = 1
+
+
